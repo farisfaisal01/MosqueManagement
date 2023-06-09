a56bec19403f2dadefebc06e4b0794ee76275a67
farisfaisal01 <farisfaisal100@gmail.com>
2023-06-09 11:22:14 +0800

    Application History

--- MosqueManagement/Controllers/DocumentController.cs
+++ MosqueManagement/Controllers/DocumentController.cs
@@ -275,5 +275,35 @@ namespace MosqueManagement.Controllers
             TempData["UpdateSuccessMessage"] = "Keputusan disahkan!";
             return RedirectToAction("Approve");
         }
+
+        public async Task<IActionResult> AdminHistory()
+        {
+            IEnumerable<Service> services = await _serviceRepository.GetAll();
+            IEnumerable<Rental> rentals = await _rentalRepository.GetAll();
+            IEnumerable<Social> socials = await _socialRepository.GetAll();
+            IEnumerable<Class> classes = await _classRepository.GetAll();
+
+            ViewData["Services"] = services;
+            ViewData["Rentals"] = rentals;
+            ViewData["Socials"] = socials;
+            ViewData["Classes"] = classes;
+
+            return View();
+        }
+
+        public async Task<IActionResult> History()
+        {
+            IEnumerable<Service> services = await _serviceRepository.GetAll();
+            IEnumerable<Rental> rentals = await _rentalRepository.GetAll();
+            IEnumerable<Social> socials = await _socialRepository.GetAll();
+            IEnumerable<Class> classes = await _classRepository.GetAll();
+
+            ViewData["Services"] = services;
+            ViewData["Rentals"] = rentals;
+            ViewData["Socials"] = socials;
+            ViewData["Classes"] = classes;
+
+            return View();
+        }
     }
 }
