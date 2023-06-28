b1cabeb841b2f9f95b6a02e6e5b03f0ac59a3cdf
farisfaisal01 <farisfaisal100@gmail.com>
2023-06-28 15:03:06 +0800

    Update User.cs

--- MosqueManagement/Models/User.cs
+++ MosqueManagement/Models/User.cs
@@ -6,13 +6,10 @@ namespace MosqueManagement.Models
     {
         [Key]
 
-        public int ? id { get; set; }
+        public int ? userId { get; set; }
         public string ? name { get; set; }
         public string ? username { get; set; }
-        public string ? ic { get; set; }
-        public string ? address { get; set; }
         public string ? email { get; set; }
-        public string ? gender { get; set; }
         public string ? phone { get; set; }
         public string ? password { get; set; }
         public string ? role { get; set; }
